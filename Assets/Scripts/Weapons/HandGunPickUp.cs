89445ef835f3ecfec101c9b0382d63852202e652
Vuduongphuoc <63326488+Vuduongphuoc@users.noreply.github.com>
2022-12-20 13:45:59 +0700

    Update Gun

---
+++ Assets/Scripts/Weapons/HandGunPickUp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGunPickUp : MonoBehaviour
+{
+    public GameObject RealHandGun;
+    public GameObject FakeHandGun;
+    public AudioSource PickUpSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        RealHandGun.SetActive(true);
+        FakeHandGun.SetActive(false);
+        PickUpSound.Play();
+    }
+}
