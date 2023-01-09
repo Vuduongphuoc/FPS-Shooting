a6bbb8a1f52cbb392201d9789e0309f69e9fa9ec
Vuduongphuoc <63326488+Vuduongphuoc@users.noreply.github.com>
2023-01-09 11:46:16 +0700

    Update AI

---
+++ Assets/Scripts/Enemy/EnemySpotting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpotting : MonoBehaviour
+{
+    public Transform thePlayer;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(thePlayer);
+    }
+}
---
+++ Assets/Scripts/Enemy/SpotPlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotPlayer : MonoBehaviour
+{
+    public string hitTag;
+    public bool lookingAtPlayer = false;
+    public GameObject EnemyBot;
+    public AudioSource firesound;
+    public bool isFiring = false;
+    public float FireRate = 1.2f;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit))
+        {
+            hitTag = Hit.transform.tag;
+            
+        }
+        if(hitTag == "Player" && isFiring == false )
+        {
+            StartCoroutine(EnemyFire());
+        }
+        if(hitTag != "Player")
+        {
+            EnemyBot.GetComponent<Animator>().Play("Idle");
+            lookingAtPlayer = false;
+        }
+
+    }
+    IEnumerator EnemyFire()
+    {
+        isFiring = true;
+        EnemyBot.GetComponent<Animator>().Play("Shooting");
+        firesound.Play();
+        lookingAtPlayer = true;
+        yield return new WaitForSeconds(FireRate);
+        isFiring = false;
+    }
+}
